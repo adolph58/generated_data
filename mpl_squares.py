e3427686c91015d96d890f1da41a5aa4f0ad7641
Arte <jiangtao.cheng@orderplus.com>
2021-08-27 15:06:55 +0800

    校正图形

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 # 设置图标标题并给坐标轴加上标签。中文显示乱码，改成英文
 ax.set_title("squares", fontsize=24)

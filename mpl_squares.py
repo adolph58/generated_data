afc2d30b1e86a07e998eab165b0a4ba81a002725
Arte <jiangtao.cheng@orderplus.com>
2021-08-27 15:00:56 +0800

    中文显示乱码，改成英文

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,10 +4,10 @@ squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
 ax.plot(squares, linewidth=3)
 
-# 设置图标标题并给坐标轴加上标签。
-ax.set_title("平方数", fontsize=24)
-ax.set_xlabel("值", fontsize=14)
-ax.set_ylabel("值的平方", fontsize=14)
+# 设置图标标题并给坐标轴加上标签。中文显示乱码，改成英文
+ax.set_title("squares", fontsize=24)
+ax.set_xlabel("values", fontsize=14)
+ax.set_ylabel("values's squares", fontsize=14)
 
 # 设置刻度标记的大小。
 ax.tick_params(axis='both', labelsize=14)

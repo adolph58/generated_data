600745a19e612dc36c454d3892e075fd368481f5
Arte <jiangtao.cheng@orderplus.com>
2021-08-30 15:24:57 +0800

    同时掷两个骰子的结果:D6+D10

--- dice_visual.py
+++ dice_visual.py
@@ -5,11 +5,11 @@ from die import Die
 
 # 创建两个 D6.
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # 掷几次骰子并将结果存储在一个列表中。
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -26,5 +26,5 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': '结果', 'dtick': 1}
 y_axis_config = {'title': '结果的频率'}
-my_layout = Layout(title='掷两个D6 1000次的结果', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='html/d6_d6.html')
+my_layout = Layout(title='掷一个D6 和一个D10 50000次的结果', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='html/d6_d10.html')

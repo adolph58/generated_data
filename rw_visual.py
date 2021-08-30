9c2e93a6a8f54daedda85fa036a3f4a71251de49
Arte <jiangtao.cheng@orderplus.com>
2021-08-30 14:31:11 +0800

    1.去掉坐标 2.渐变显示

--- rw_visual.py
+++ rw_visual.py
@@ -2,11 +2,28 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# 创建一个 RandomWalk 实例。
-rw = RandomWalk()
-rw.fill_walk()
-# 将所有的点都绘制出来。
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values,  rw.y_values, s=15)
-plt.show()
+while True:
+    # 创建一个 RandomWalk 实例。
+    # 初始值：5000 个点，s=15
+    rw = RandomWalk(50_000)
+    rw.fill_walk()
+
+    # 将所有的点都绘制出来。
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values,  rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # 突出起点和终点。
+    ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # 隐藏坐标轴。
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
